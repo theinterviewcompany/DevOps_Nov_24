874a9825303b44cf2fb4a84d9fecd95b0105b455
EC2 Default User <ec2-user@ip-172-31-37-142.ap-northeast-3.compute.internal>
2024-12-13 16:15:22 +0000

    this is commit message in VI editor
    with multiple lines

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Assigning a value to a variable
-echo "Please enter your name"
+#echo "Please enter your name"
 #read name
-echo "Please enter your age"
-read name age
-echo "The name is $name and the age is $age"
+#echo "Please enter your age"
+#read name age
+#echo "The name is $name and the age is $age"
